3e0d57cb30a6c8ca4cdb4c3e12ef6e87dbdad920
Wen Jie <wenjiens15@gmail.com>
2018-09-07 02:45:14 +0800

    put method

--- express/index.js
+++ express/index.js
@@ -18,25 +18,21 @@ app.get('/', (req,res) => {
 
 
 app.get('/api/courses/:id',(req,res) => {
-    const result = courses.find(course => course.id === parseInt(req.params.id));
-
-    if(!result)
+    const course = courses.find(course => course.id === parseInt(req.params.id));
+    if(!course)
         res.status(404).send('wrong id')
     
     res.send(result);
 });
 
+
 app.post('/api/courses',(req, res) => {
-    const schema= {
-        name: Joi.string().min(3).required()
-    };
-    const result = Joi.validate(req.body,schema);
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
+    const {error} = validateCourse(req.body);
+    if(error) {
+        res.status(400).send(error.details[0].message);
         return;
     }
     
-
     const course = {
         id: courses.length +1,
         name: req.body.name
@@ -45,6 +41,31 @@ app.post('/api/courses',(req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+
+app.put('/api/courses/:id',(req,res) =>{
+    const course = courses.find(course => course.id === parseInt(req.params.id));
+    if(!course)
+        res.status(404).send('wrong id');
+    
+    const {error} = validateCourse(req.body);
+    if(error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course,schema);
+}
 
 
 const port = process.env.PORT || 3000;
