77402b45fed31f7e80a13f79d5b3eb8c5f48e5c2
Wen Jie <wenjiens15@gmail.com>
2018-09-07 02:54:43 +0800

    fixing bugs to exit if id is wrong

--- express/index.js
+++ express/index.js
@@ -29,8 +29,7 @@ app.get('/api/courses/:id',(req,res) => {
 app.post('/api/courses',(req, res) => {
     const {error} = validateCourse(req.body);
     if(error) {
-        res.status(400).send(error.details[0].message);
-        return;
+        return res.status(400).send(error.details[0].message);
     }
     
     const course = {
@@ -45,13 +44,13 @@ app.post('/api/courses',(req, res) => {
 
 app.put('/api/courses/:id',(req,res) =>{
     const course = courses.find(course => course.id === parseInt(req.params.id));
-    if(!course)
-        res.status(404).send('wrong id');
+    if(!course){
+        return res.status(404).send('wrong id');
+    }
     
     const {error} = validateCourse(req.body);
     if(error) {
-        res.status(400).send(error.details[0].message);
-        return;
+        return res.status(400).send(error.details[0].message);
     }
 
     course.name = req.body.name;
@@ -62,7 +61,7 @@ app.put('/api/courses/:id',(req,res) =>{
 app.delete('/api/courses/:id',(req,res) => {
     const course = courses.find(course => course.id === parseInt(req.params.id));
     if(!course)
-        res.status(404).send('wrong id');
+        return res.status(404).send('wrong id');
     
     const index = courses.indexOf(course);
     courses.splice(index,1);
