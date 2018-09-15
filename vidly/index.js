decda2611da0daba69b76396cab3d143863e9957
Wen Jie <wenjiens15@gmail.com>
2018-09-16 03:56:53 +0800

    update genres to mongodb

--- vidly/index.js
+++ vidly/index.js
@@ -1,10 +1,15 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const genres = require('./routes/genres');
+
 const app = express();
 
-app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/vidly',{ useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB..'))
+    .catch(err => console.error('Cannot connect to db... ',err));
 
+app.use(express.json());
 app.use('/api/genres', genres);
 
 const port = process.env.PORT || 3000;
